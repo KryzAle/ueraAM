869107e3e3d78a4cc3d74a9a3bfe85ca0b0cd0c1
Cristian Cabezas <kryzale@gmail.com>
2020-05-31 02:45:45 -0500

    Low Fix Vista Editar Datos Estudiante

--- ueraAM/resources/views/estudiantes/editar.blade.php
+++ ueraAM/resources/views/estudiantes/editar.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
+@php
+    $usuarioActivo = auth()->id();
+@endphp
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-9">
